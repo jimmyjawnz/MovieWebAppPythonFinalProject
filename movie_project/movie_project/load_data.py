ab6fa007cf3b51dfa61a70be6d72aee9171436f8
james <jamesprovee@gmail.com>
2025-11-27 09:47:50 -0700

    Created script to read csv files and convert them into movie objects and database rows

---
+++ movie_project/movie_project/load_data.py
@@ -0,0 +1,25 @@
+import pandas as pd
+from movie_app.models import Movies
+
+# Loads the Movies/Shows from the netflix titles csv file
+# into the database
+def load_movie_data():
+    df = pd.read_csv("movie_app/netflix_titles.csv")
+    for _, row in df.iterrows():
+        Movies.objects.create(
+            show_id=row["show_id"],
+            type=row["type"],
+            title=row["title"],
+            director=row["director"],
+            cast=row["cast"],
+            country=row["country"],
+            date_added=row["date_added"],
+            release_year=row["release_year"],
+            age_rating=row["rating"],
+            duration=row["duration"],
+            listed_in=row["listed_in"],
+            description=row["description"]
+        )
+    print("Data Loaded Successfully!")
+
+load_movie_data()
