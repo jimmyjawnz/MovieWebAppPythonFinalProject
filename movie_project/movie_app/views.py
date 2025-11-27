df03646c502ceea38b4afdd0d86ed512ff25a4cc
james <jamesprovee@gmail.com>
2025-11-27 10:07:38 -0700

    Added Search Page to simulate searching movies and shows

--- movie_project/movie_app/views.py
+++ movie_project/movie_app/views.py
@@ -1,3 +1,25 @@
 from django.shortcuts import render
+from movie_app.models import Movies
 
-# Create your views here.
+
+# Search View
+def search_population(req):
+    query = req.GET.get('q', '')
+    year_filter = req.GET.get('year', '')
+
+    results = Movies.objects.all()
+
+    if query:
+        results = results.filter(title__icontains=query)
+    
+    if year_filter:
+        results = results.filter(release_year=year_filter)
+    
+    years = Movies.objects.values_list('release_year', flat=True).distinct()
+
+    return render(req, 'search.html', {
+        'results': results,
+        'query': query,
+        'years': years,
+        'selected_year': year_filter
+    })
