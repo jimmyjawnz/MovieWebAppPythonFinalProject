021a6b383d16d7397ac1a3bfe1f2d4129fa9fc6d
james <jamesprovee@gmail.com>
2025-11-27 12:00:24 -0700

    Added Countries as a search option

--- movie_project/movie_app/views.py
+++ movie_project/movie_app/views.py
@@ -9,6 +9,7 @@ def home_page(req):
 def search_population(req):
     query = req.GET.get('q', '')
     year_filter = req.GET.get('year', '')
+    country_filter = req.GET.get('country', '')
 
     results = Movies.objects.all().order_by('title')
 
@@ -17,12 +18,24 @@ def search_population(req):
     
     if year_filter:
         results = results.filter(release_year=year_filter)
+
+    if country_filter:
+        results = results.filter(country=country_filter)
     
     years = Movies.objects.values_list('release_year', flat=True).distinct().order_by('-release_year')
+
+    # sets up countries as an option
+    countries = Movies.objects.values_list('country', flat=True).distinct().order_by('country')
+    countries = [word for sentence in countries for word in sentence.replace(',', '').split()]
+    countries = set(countries)
+    countries = list(countries)
+    countries.sort()
 
     return render(req, 'search.html', {
         'results': results,
         'query': query,
         'years': years,
-        'selected_year': year_filter
+        'selected_year': year_filter,
+        'countries': countries,
+        'selected_country': country_filter
     })
